e100b13cfdf46f91cdbe8b63c37ba7abe97ac685
Dane Murphy <danem7@gmail.com>
2021-07-12 00:59:12 -0700

    starting over from v1.0.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.2.3
+VERSION=1.0.0
 
 compile:
 	go build -o _output/ping .
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"architecture/routes"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,36 +13,5 @@ func main() {
 	r.GET("/post/:id", routes.GetPost)
 	r.GET("/healthz", routes.Healthz)
 
-	feature1()
-	feature2()
-	feature3()
-	feature4()
-	feature5()
-	feature6()
-
 	r.Run()
-}
-
-func feature1() {
-	fmt.Println("Feature 1 activated v1.2.1 activated")
-}
-
-func feature2() {
-	fmt.Println("Feature 2 activated v1.2.2 activated")
-}
-
-func feature3() {
-	fmt.Println("Feature 3 for v1.2.3 activated")
-}
-
-func feature4() {
-	fmt.Println("Feature 4 for v1.2.3 activated")
-}
-
-func feature5() {
-	fmt.Println("Feature 5 for v1.2.3 activated")
-}
-
-func feature6() {
-	fmt.Println("Feature 6 for v1.2.3 activated")
 }
