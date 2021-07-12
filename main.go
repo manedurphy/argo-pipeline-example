71475680dbcb0049b7cad06fe0b630d17fd2cdce
Dane Murphy <67260328+manedurphy@users.noreply.github.com>
2021-07-12 00:34:43 -0700

    v1.2.3 ready for production

--- main.go
+++ main.go
@@ -18,22 +18,32 @@ func main() {
 	feature2()
 	feature3()
 	feature4()
+	feature5()
+	feature6()
 
 	r.Run()
 }
 
 func feature1() {
-	fmt.Println("Feature 1 activated")
+	fmt.Println("Feature 1 activated v1.2.1 activated")
 }
 
 func feature2() {
-	fmt.Println("Feature 2 activated")
+	fmt.Println("Feature 2 activated v1.2.2 activated")
 }
 
 func feature3() {
-	fmt.Println("Feature for v1.2.3 activated")
+	fmt.Println("Feature 3 for v1.2.3 activated")
 }
 
 func feature4() {
-	fmt.Println("Feature for v1.2.3 activated")
+	fmt.Println("Feature 4 for v1.2.3 activated")
+}
+
+func feature5() {
+	fmt.Println("Feature 5 for v1.2.3 activated")
+}
+
+func feature6() {
+	fmt.Println("Feature 6 for v1.2.3 activated")
 }
