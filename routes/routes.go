701566decba60149cac2490e0e3a15e1aac5bac0
Dane Murphy <67260328+manedurphy@users.noreply.github.com>
2021-07-11 21:30:24 -0700

    getting data from external api (#10)
    * release workflow is live (#9)
    
    * getting post data from external api

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,19 @@
 package routes
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Post struct {
+	UserId int
+	Id     int
+	Title  string
+	Body   string
+}
 
 func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
@@ -12,8 +21,39 @@ func Ping(c *gin.Context) {
 	})
 }
 
+func GetPost(c *gin.Context) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+
+	if err != nil {
+		internalServerError(c)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		internalServerError(c)
+		return
+	}
+
+	var post Post
+	json.Unmarshal(body, &post)
+
+	c.JSON(http.StatusOK, gin.H{
+		"post": post,
+	})
+}
+
 func Healthz(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "healthy",
+	})
+}
+
+func internalServerError(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"message": "internal server error",
 	})
 }
